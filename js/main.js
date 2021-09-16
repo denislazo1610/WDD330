d63b6d583e41f040de2653f7c80a6d1d10d94534
Denis Lazo <denislazo1610@gmail.com>
2021-09-15 20:38:52 -0600

    How to add one il to the ol element

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ const links = [
         url: "week1/index.html"
     }
 ]
+
+var list = document.querySelector('ol');
+
+var li = document.createElement('li');
+
+li.appendChild(document.createTextNode("Week1 Notes"));
+
+list.appendChild(li);
