a71df6c55a2f2fda870c6c59fe334f3607d3d3a5
Denis Lazo <denislazo1610@gmail.com>
2021-09-20 22:37:58 -0600

    Add info in the main JS to add week2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,25 @@
-'use strict';
+"use strict";
 
 const block1 = [
-    {
-        label: "Week1 notes",
-        url: "week1/index.html"
-    }
-]
+  {
+    label: "Week1 notes",
+    url: "week1/index.html",
+  },
+  {
+    label: "Week2 notes",
+    url: "week2/index.html",
+  },
+];
 
-const block2 = [
-    {
+const block2 = [{}];
 
-    }
-]
+var list1 = document.querySelector("ol#first");
 
-var list1 = document.querySelector('ol#first');
-
-
-
-for(var number = 0; number < block1.length; number++){
-    var li = document.createElement('li');
-    var a = document.createElement('a');
-    a.innerHTML = block1[number].label;
-    a.href = block1[number].url;
-    li.appendChild(a);
-    list1.appendChild(li);
+for (var number = 0; number < block1.length; number++) {
+  var li = document.createElement("li");
+  var a = document.createElement("a");
+  a.innerHTML = block1[number].label;
+  a.href = block1[number].url;
+  li.appendChild(a);
+  list1.appendChild(li);
 }
